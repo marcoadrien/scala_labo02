e9b66807ffbde8ff3970d8f21504f4b5e6575fdb
marcoadrien <adrien.marco@heig-vd.ch>
2018-04-10 09:35:02 +0200

    ajout des noeuds, en attente de certaines réponses de Miguel pour les détails

--- lab02/src/calculator/parser/Trees.scala
+++ lab02/src/calculator/parser/Trees.scala
@@ -15,7 +15,33 @@ object Trees {
   /** lhs: left hand side, rhs: right hand side */
   case class Assign(ident: Identifier, value: ExprTree) extends ExprTree
   case class Plus(lhs: ExprTree, rhs: ExprTree) extends ExprTree
-  ???
+  //--------------------------------------------------------------------------------------------------------------------
+  case class Minus(lhs: ExprTree, rhs: ExprTree) extends ExprTree
+  case class Times(lhs: ExprTree, rhs: ExprTree) extends ExprTree
+  case class Div(lhs: ExprTree, rhs: ExprTree) extends ExprTree
+  case class Mod(lhs: ExprTree, rhs: ExprTree) extends ExprTree
+  case class Pow(lhs: ExprTree, rhs: ExprTree) extends ExprTree
+
+  //???????????????????????
+  case class Fact(lhs: NumLit) extends ExprTree
+  case class Lparen(rhs: ExprTree) extends ExprTree
+  case class Rparen(lhs: ExprTree) extends ExprTree
+  case class Comma(lhs: ExprTree, rhs: ExprTree) extends ExprTree
+
+  //???????????????????????
+  case class Sqrt(rhs: ExprTree) extends ExprTree
+  case class Gcd(rhs1: ExprTree, rhs2: ExprTree) extends ExprTree
+  case class Egcd(rhs1: ExprTree,
+                  rhs2: ExprTree,
+                  rhs3: ExprTree = NumLit("1"),
+                  rhs4: ExprTree = NumLit("0"),
+                  rhs5: ExprTree = NumLit("0"),
+                  rhs6: ExprTree = NumLit("1")) extends ExprTree
+  case class ModInv(rhs1: ExprTree, rhs2: ExprTree) extends ExprTree
+
+  //prime et quadratic solve ne sont pas à implémenter selon la consigne => il faudrales retirer de Tokens.scala et Lexer.scala!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  //--------------------------------------------------------------------------------------------------------------------
+
 
   /** Leaves Expression Trees */
   case class NumLit(value: String) extends ExprTree
