4bf145ba591e386d5d784d9da6b5736f37a1d9ae
Loan Lassalle <18124884+lassalleloan@users.noreply.github.com>
2018-04-17 20:07:27 +0200

    Test of Floating-point arithmetic

--- lab02/src/calculator/Calculator.scala
+++ lab02/src/calculator/Calculator.scala
@@ -9,10 +9,14 @@ import scala.io.Source
 
 class Calculator(source: Source) extends Parser(source: Source) {
 
+  val scale = 10
+
  // TODO: Utiliser des constantes pour définir une réponse
   def execute(): Unit = computeSource match {
     case Double.NegativeInfinity => println("Memory updated !")
-    case result => println("Result : " + Utils.normalize(result.toString))
+    case result =>
+      val resultNormalized = BigDecimal(result).setScale(scale, BigDecimal.RoundingMode.HALF_UP)
+      println("Result : " + Utils.normalize(resultNormalized.toString))
   }
 
 }
